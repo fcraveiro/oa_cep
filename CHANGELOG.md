708db63b238209ecbeee8f0ebb59492eb3d84517
fcraveiro <dextruz@hotmail.com>
2022-01-28 15:21:08 -0300

    Release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0 
+
+* Primeiro release
+
 ## 0.0.1
 
-* TODO: Describe initial release.
+* Inicio
--- test/oa_cep_test.dart
+++ test/oa_cep_test.dart
@@ -1,15 +1,4 @@
 
 /*
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:oa_cep/oa_cep.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
 */
